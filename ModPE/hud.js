6cbabcd06e31f49d5ecab10770220cc4c4a2ac63
eDroid <edroiiid@gmail.com>
2017-02-09 07:54:42 -0800

    Update hud.js

--- ModPE/hud.js
+++ ModPE/hud.js
@@ -1,1 +1,24 @@
-
+/**
+* ModPEAddon:
+* ModPE.hud
+* eDroid
+* http://github.edroidthedev.com/?repo=ModPEAddon/ModPE/hud.js
+*/
+ModPE.hud = function(run, msg){
+  msg = msg || "";
+  function hudTask(){
+    ctx.runOnUiThread(new java.lang.Runnable({
+      run: function(){
+        new android.os.Handler().postDelayed(new java.lang.Runnable({
+          run: function() {
+            if(run){
+              ModPE.showTipMessage(msg)
+            };
+            eval(hudTask())
+          }
+        }), 1000 / 70);
+      }
+    }))
+  }
+  hudTask();
+};
