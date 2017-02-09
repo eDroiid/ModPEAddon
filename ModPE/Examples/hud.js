4aa25289bb623ee69db1752937f58ecce5813cb0
eDroid <edroiiid@gmail.com>
2017-02-09 10:31:38 -0800

    Create hud.js

---
+++ ModPE/Examples/hud.js
@@ -0,0 +1,11 @@
+/**
+* ModPEAddon Example:
+* ModPE.hud
+* eDroid
+* http://github.edroidthedev.com/?repo=ModPEAddon/ModPE/hud.js
+*/
+function newLevel(){
+  print("Hud Starting");
+  var message = "Hola!";
+  ModPE.hud(true, message);
+}
